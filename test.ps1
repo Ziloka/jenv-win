0184012a5f3d9adf422aed464841dd91fd5c7b45
572512230701006851 <connorwu1010@gmail.com>
2021-03-02 21:33:29 -0500

    Revert "Research on architectures"
    This reverts commit 5d0845c7e138989ffffede36206afc76473e384f.

--- test.ps1
+++
@@ -1,2 +0,0 @@
-$config = ConvertFrom-Json -InputObject (Get-Content -Raw -Encoding "UTF8" -Path ([String]::Format("config.json")));
-Write-Output $config.architectures.($env:PROCESSOR_ARCHITECTURE)
